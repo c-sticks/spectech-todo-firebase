e28f267e4ec474446fbee2ac2b2d4823663773a7
Hironori Wada <hironori104@gmail.com>
2024-04-03 14:47:26 +0900

    done: read & write

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,22 @@
 import { useState } from "react";
 import "./App.css";
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; // この行を追加
+import { doc, setDoc, getDoc } from "firebase/firestore"; // この行を追加 2
+
+const firebaseConfig = {
+  
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app); // この行を追加
+await setDoc(doc(db, "cities", "new-city-id"), {
+  hoge: "xxxx",
+});
+const docSnap = await getDoc(doc(db, "cities", "new-city-id"));
+const docData = docSnap.data();
+
 function getTasks() {
   const tasksString = localStorage.getItem("tasks");
   if (tasksString != null) {
@@ -23,6 +39,11 @@ function App() {
     const newTasks = [...tasks, text];
     setTasks(newTasks);
     saveTasks(newTasks);
+
+    // これを追加 2
+    setDoc(doc(db, "cities", "new-city-id"), {
+      hoge: "aaaoa",
+    });
   };
 
   return (
@@ -33,6 +54,8 @@ function App() {
         }}
       />
       <button onClick={onClick}>追加</button>
+
+      {JSON.stringify(docData)}
 
       {tasks.map((task) => (
         <div>
