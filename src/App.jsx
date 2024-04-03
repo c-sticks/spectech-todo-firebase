680f59293f1e56c63ac9a9935dd6807fe07938e1
Hironori Wada <hironori104@gmail.com>
2024-04-03 15:13:28 +0900

    task write

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,43 @@
 /* eslint-disable react/jsx-key */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // この行を追加
-import { doc, setDoc, getDoc } from "firebase/firestore"; // この行を追加 2
+import { getFirestore } from "firebase/firestore";
+import { doc, setDoc, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app); // この行を追加
-await setDoc(doc(db, "cities", "new-city-id"), {
-  hoge: "xxxx",
-});
-const docSnap = await getDoc(doc(db, "cities", "new-city-id"));
-const docData = docSnap.data();
+const db = getFirestore(app);
 
-function getTasks() {
-  const tasksString = localStorage.getItem("tasks");
-  if (tasksString != null) {
-    return JSON.parse(tasksString);
-  } else {
+async function getTasks() {
+  const docSnap = await getDoc(doc(db, "tasks", "mytask"));
+  const docData = docSnap.data();
+  if (docData == null) {
     return [];
   }
-}
-
-function saveTasks(tasks) {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+  return docData["tasks"];
 }
 
 function App() {
   const [text, setText] = useState("");
-  const [tasks, setTasks] = useState(getTasks());
+  const [tasks, setTasks] = useState([]);
 
-  const onClick = () => {
+  useEffect(() => {
+    getTasks().then((fetched) => {
+      console.log({ fetched });
+      setTasks(fetched);
+    });
+  }, []);
+
+  const onClick = async () => {
     const newTasks = [...tasks, text];
     setTasks(newTasks);
-    saveTasks(newTasks);
-
-    // これを追加 2
-    setDoc(doc(db, "cities", "new-city-id"), {
-      hoge: "aaaoa",
+    await setDoc(doc(db, "tasks", "mytask"), {
+      tasks: newTasks,
     });
   };
 
@@ -54,9 +49,6 @@ function App() {
         }}
       />
       <button onClick={onClick}>追加</button>
-
-      {JSON.stringify(docData)}
-
       {tasks.map((task) => (
         <div>
           <input type="checkbox" />
